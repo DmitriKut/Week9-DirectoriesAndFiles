7da3a5cf04029a32c8f3607a7e75b0c602196b6c
Dmitri <dimaskut@gmail.com>
2023-01-10 10:52:47 +0200

    control flow

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+
+
+			if (File.Exists($"C:\\Users\\atrei\\samples\\shoppingList\\myShoppingList.txt"))
+			{
+				Console.WriteLine($"File myShoppingList.txt is exist.");
+			}
+			else
+			{
+				string rootDirectory = @"C:\Users\atrei\samples";
+				Console.WriteLine("Enter directory name: (recommended shoppingList)");
+				string newDirectory = Console.ReadLine();
+				Console.WriteLine("Enter file name: (recommended myShoppingList.txt)");
+				string fileName = Console.ReadLine();
+				Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+				File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+
+				Console.WriteLine($"{rootDirectory}\\{newDirectory}\\{fileName} is created");
+			}
+
+
+		}
+
+	}
+}
---
+++ myShoppingList/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace myShoppingList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (File.Exists($"C:\\Users\\atrei\\samples\\shoppingList\\myShoppingList.txt"))
+			{
+				string[] arrayFromFile = File.ReadAllLines($"C:\\Users\\atrei\\samples\\shoppingList\\myShoppingList.txt");
+				List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+				bool loopActive = true;
+
+				while (loopActive)
+				{
+					Console.WriteLine("Would you like to add a item to shoppinglist? Y/N:");
+					char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+					if (userInput == 'y')
+					{
+						Console.WriteLine("Enter you item:");
+						string useritem = Console.ReadLine();
+						myShoppingList.Add(useritem);
+
+					}
+					else
+					{
+						loopActive = false;
+						Console.WriteLine("Take care!");
+					}
+
+				}
+				Console.Clear();
+
+				foreach (string items in myShoppingList)
+				{
+					Console.WriteLine($"Your wish: {items}");
+				}
+
+				File.WriteAllLines($"C:\\Users\\atrei\\samples\\shoppingList\\myShoppingList.txt", myShoppingList);
+			}
+			else
+			{
+				Console.WriteLine("Take care!");
+			}
+		}
+	}
+}
